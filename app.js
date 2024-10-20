487d6584f0fc2a2cfff27ead12023e31a7cc99cf
VicScorcesee <victormelvin@my.tridenttech.edu>
2024-10-20 00:13:43 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "GoodChuckAPI_KEY",
+    authDomain: "OUR_PROJECT_ID.firebaseapp.com",
+    projectId: "GOOD_CHUCK_ID",
+    storageBucket: "GOOD_CHUCK_ID.appspot.com",
+    messagingSenderId: "GOOD_CHUCK_ID",
+    appId: "GOOD_CHUCK_ID"
+};
+firebase.initializeApp(firebaseConfig);
